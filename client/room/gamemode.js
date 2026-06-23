7c6aa3681a310e16c867d60657a4820900cdbcb5
TestPixelcombats2 <130068274+Zaur776@users.noreply.github.com>
2026-06-24 03:43:11 +0500

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,62 +1,14 @@
-var room = require('pixel_combats/room');
+import { GameMode, Teams, Players } from 'pixel_combats/room';
 
-function set_inventory() {
-    var context = room.Inventory.GetContext();
-    context.Main.Value = false;
-    context.Secondary.Value = false;
-    context.Melee.Value = false;
-    context.Explosive.Value = false;
-    context.Build.Value = false;
-}
-
-function set_build_settings() {
-    var context = room.Build.GetContext();
-    context.Pipette.Value = true;
-    context.BalkLenChange.Value = true;
-    context.SetSkyEnable.Value = true;
-    context.GenMapEnable.Value = true;
-    context.ChangeCameraPointsEnable.Value = true;
-    context.QuadChangeEnable.Value = true;
-    context.BuildModeEnable.Value = true;
-    context.CollapseChangeEnable.Value = true;
-    context.RenameMapEnable.Value = true;
-    context.ChangeMapAuthorsEnable.Value = true;
-    context.LoadMapEnable.Value = true;
-    context.ChangeSpawnsEnable.Value = true;
-    context.BlocksSet.Value = room.BuildBlocksSet.AllClear;
-}
-
-function apply_room_options() {
-    var gameModeParameters = room.GameMode.Parameters;
-    var buildContext = room.Build.GetContext();
-    buildContext.FloodFill.Value = gameModeParameters.GetBool("FloodFill");
-    buildContext.FillQuad.Value = gameModeParameters.GetBool("FillQuad");
-    buildContext.RemoveQuad.Value = gameModeParameters.GetBool("RemoveQuad");
-    buildContext.FlyEnable.Value = gameModeParameters.GetBool("Fly");
-    room.Damage.GetContext().DamageOut.Value = gameModeParameters.GetBool("Damage");
-    room.BreackGraph.OnlyPlayerBlocksDmg = gameModeParameters.GetBool("PartialDesruction");
-    room.BreackGraph.WeakBlocks = gameModeParameters.GetBool("LoosenBlocks");
-}
-
-room.GameMode.OnStart.Add(function() {
-    room.Properties.GetContext().GameModeName.Value = "GameModes/Peace";
-    room.Ui.GetContext().Hint.Value = "Hint/BuildBase";
-    room.Ui.GetContext().QuadsCount.Value = true;
-    room.BreackGraph.BreackAll = true;
-    room.Spawns.GetContext().RespawnTime.Value = 0;
-    
-    set_build_settings();
-    set_inventory();
-    apply_room_options();
-
-    var redTeam = room.Teams.Get("Red");
-    var blueTeam = room.Teams.Get("Blue");
+GameMode.OnStart.Add(function() {
+    const redTeam = Teams.Get("Red");
+    const blueTeam = Teams.Get("Blue");
     if (redTeam != null) redTeam.AutoBalance.Value = false;
     if (blueTeam != null) blueTeam.AutoBalance.Value = false;
 });
 
-room.Teams.OnAddTeam.Add(function (team) {
-    if (team.id === "Blue") {
+Teams.OnAddTeam.Add(function (team) {
+    if (team.Id === "Blue") {
         team.Inventory.Main.Value = false;
         team.Inventory.Secondary.Value = false;
         team.Inventory.Melee.Value = false;
@@ -65,13 +17,14 @@ room.Teams.OnAddTeam.Add(function (team) {
         team.Inventory.BuildInfinity.Value = false;
     }
     else {
+        team.Inventory.Main.Value = false;
+        team.Inventory.Secondary.Value = false;
         team.Inventory.Melee.Value = true;
-        team.Inventory.Build.Value = true;
-        team.Inventory.BuildInfinity.Value = true;
+        team.Inventory.Build.Value = false;
     }
 });
 
-room.Players.OnPlayerSpawn.Add(function (player) {
+Players.OnPlayerSpawn.Add(function (player) {
     if (player.Id === "EBBD6F896A740312") {
         player.Build.Fly.Value = true;
         player.Inventory.Main.Value = true;
@@ -84,7 +37,7 @@ room.Players.OnPlayerSpawn.Add(function (player) {
         return;
     }
 
-    if (player.Team != null && player.Team.id === "Blue") {
+    if (player.Team != null && player.Team.Id === "Blue") {
         player.Build.Fly.Value = false;
         player.Inventory.Main.Value = false;
         player.Inventory.Secondary.Value = false;
@@ -92,22 +45,29 @@ room.Players.OnPlayerSpawn.Add(function (player) {
         player.Inventory.Explosive.Value = false;
         player.Inventory.Build.Value = false;
         player.Damage.FriendlyFire.Value = false;
+        player.Damage.DamageOut.Value = true;
     }
     
-    if (player.Team != null && player.Team.id === "Red") {
+    if (player.Team != null && player.Team.Id === "Red") {
+        player.Build.Fly.Value = false;
+        player.Inventory.Main.Value = false;
+        player.Inventory.Secondary.Value = false;
+        player.Inventory.Melee.Value = true;
+        player.Inventory.Explosive.Value = false;
+        player.Inventory.Build.Value = false;
         player.Damage.DamageOut.Value = false;
     }
 });
 
-room.Teams.OnRequestJoinTeam.Add(function (player, team) {
+Teams.OnRequestJoinTeam.Add(function (player, team) {
     if (player.Id === "EBBD6F896A740312") {
         team.Add(player);
         return;
     }
-    var targetTeam = room.Teams.Get("Red");
+    const targetTeam = Teams.Get("Red");
     if (targetTeam != null) {
         targetTeam.Add(player);
     }
 });
 
-room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
+Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
